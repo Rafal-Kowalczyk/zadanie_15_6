8024c248733fe0b3114d234f33f71ab95c176597
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-27 21:38:00 +0100

    Add files code

---
+++ script-compiled.js
@@ -0,0 +1,75 @@
+// klasa Stopwatch
+
+class Stopwatch {
+				constructor(display) {
+								this.running = false;
+								this.display = display;
+								this.reset();
+								this.print(this.times);
+				}
+				// metoda reset    
+				reset() {
+								this.times = {
+												minutes: 0,
+												seconds: 0,
+												miliseconds: 0
+								};
+				}
+				// metoda print
+				print() {
+								this.display.innerText = this.format(this.times);
+				}
+				// metoda format - przygotowywuje tekst do wyświetlenia 
+				format(times) {
+								return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+				}
+
+				// implementacja funkcji start
+				start() {
+								if (!this.running) {
+												this.running = true;
+												this.watch = setInterval(() => this.step(), 10);
+								}
+				}
+				// metoda step - sprawdza czy timer jest uruchomiony
+				step() {
+								if (!this.running) return;
+								this.calculate();
+								this.print();
+				}
+				// metoda calculate - przelicza min, sek i milisek
+				calculate() {
+								this.times.miliseconds += 1;
+								if (this.times.miliseconds >= 100) {
+												this.times.seconds += 1;
+												this.times.miliseconds = 0;
+								}
+								if (this.times.seconds >= 60) {
+												this.times.minutes += 1;
+												this.times.seconds = 0;
+								}
+				}
+				// metoda stop - ztrzymanie timera
+				stop() {
+								this.running = false;
+								clearInterval(this.watch);
+				}
+}
+// implementacja funkcji pad0 - dodającej zero do liczbjednocyfrowych
+function pad0(value) {
+				let result = value.toString();
+				if (result.length < 2) {
+								result = '0' + result;
+				}
+				return result;
+}
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+
+// metody przycisków
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
